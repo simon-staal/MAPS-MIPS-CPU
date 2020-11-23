df5c15c4953f0a6581df855239f020d8092a0465
sts219 <sts219@ic.ac.uk>
2020-11-23 16:56:34 +0000

    Added instruction formats

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -14,15 +14,19 @@ module mips_cpu_bus(
     output logic[3:0] byteenable,
     output logic[31:0] readdata
   );
-
+    /* Instruction formats:
+    R-type: opcode (6) source1 (5) source2 (5) dest (5) shift (5) function (5)
+    I-tyoe: opcode (6) source (5) dest (5) imm (16)
+    J-type: opcode (6) mem (26)
+    */
     typedef enum logic[5:0] {
         OPCODE_R = 6'b00000, // Register type instructions
         OPCODE_ADDIU = 6'b001001,
         OPCODE_ANDI = 6'b001100,
 
-    } opdocde_t;
+    } opcode_t;
 
     typedef enum logic[5:0] {
-        FUNCTION_ADDU = 6'b100001,
+        FUNCTION_ADDU = 6'b100001, //$s1 = $s2 + $s3
         FUNCTION_AND = 6'b100100,
     } function_t;
