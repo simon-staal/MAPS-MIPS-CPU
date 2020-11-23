755db28986fd313b39b02378ef7c569508cab56f
sts219 <sts219@ic.ac.uk>
2020-11-23 16:43:52 +0000

    added opcode and function

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -13,4 +13,16 @@ module mips_cpu_bus(
     output logic[31:0] writedata,
     output logic[3:0] byteenable,
     output logic[31:0] readdata
-  )
+  );
+
+    typedef enum logic[5:0] {
+        OPCODE_R = 6'b00000, // Register type instructions
+        OPCODE_ADDIU = 6'b001001,
+        OPCODE_ANDI = 6'b001100,
+
+    } opdocde_t;
+
+    typedef enum logic[5:0] {
+        FUNCTION_ADDU = 6'b100001,
+        FUNCTION_AND = 6'b100100,
+    } function_t;
