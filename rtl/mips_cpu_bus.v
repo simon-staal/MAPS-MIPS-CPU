282d8a0f0c95c84b1cad6091cda2c86a10b2cc0f
sts219 <sts219@ic.ac.uk>
2020-11-24 17:35:50 +0000

    Merged adam_opcodes

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -36,11 +36,20 @@ module mips_cpu_bus(
         OPCODE_BGTZ = 6'b000111, //if(rs > 0) then pc <= pc + imm>>2 (rt == 00000)
         OPCODE_BLEZ = 6'b000110, //if(rs <= 0) then pc <= pc + imm>>2 (rt == 00000)
         OPCODE_BNE = 6'b000101 //if(rs != rt) then pc <= pc + imm>>2
+        FUNCTION_SLTU = 6'b101011, // $rd := $rs < $rt. Unsigned less-than comparison.
+      	FUNCTION_SRA = 6'b000011, // $rd := rt >> c. Arithmetic shift right by c bits.
+      	FUNCTION_SRAV = 6'b000111, // $rd := $rt >> $rs. Variable Arithmetic shift right, i.e. by a register variable.
+      	FUNCTION_SRL = 6'b000010, // $rd := $rt >> c. Logical shift right by constant c bits.
+      	FUNCTION_SRLV = 6'b000110, // $rd := $rt >> $rs. Variable logical shift right, i.e. by a register variable
+      	FUNCTION_SUBU = 6'b100011, // $rd := $rs - $rt. Subtract 2 registers.
+      	FUNCTION_XOR = 6'b100110 // $rd := $rs XOR $rt. Logical XOR between $rs and $rt.
     } opcode_t;
 
     typedef enum logic[5:0] {
         FUNCTION_ADDU = 6'b100001, //rd = rs + rt (shift = 0)
         FUNCTION_AND = 6'b100100, //rd = rs & rt (shift = 0)
+        OPCODE_SW = 6'b101011, // memory[base+offset] := $rt. Stores register rt in memory with an offset.
+      	OPCODE_XORI = 6'b001110 // $rt := $rs XORI c. Logical XOR between $rs and constant c.
     } function_t;
 
     //This logic is used for the rt field of instructions with the REGIMM opcode
