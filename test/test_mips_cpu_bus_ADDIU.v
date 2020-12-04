0972e1c75b17da4aac60ecd87ac91ef2f570db2e
sts219 <sts219@ic.ac.uk>
2020-12-04 17:49:36 +0000

    Added assembly code explanantion to ADDIU_1

--- test/test_mips_cpu_bus_ADDIU.v
+++ test/test_mips_cpu_bus_ADDIU.v
@@ -34,6 +34,13 @@ module mips_cpu_bus_tb;
         $fatal(100, "%s %s Fail Simulation did not finish within %d cycles.", TESTCASE_ID, INSTRUCTION, TIMEOUT_CYCLES);
     end
 
+    /*
+    Assembly code:
+    lw v1 0x1(zero) (loades value at address==1 into v1)
+    jr zero (jumps to address==0)
+    addiu v1 v0 0x0 (delay slot: v0 = v1 + 0)
+    */
+
     initial begin
         reset <= 0;
 
@@ -51,7 +58,7 @@ module mips_cpu_bus_tb;
         assert(byteenable==4'b1111) else $fatal(105, "%s %s Fail CPU incorrectly set byteenable %b." TESTCASE_ID, INSTRUCTION, byteenable);
 
         @(posedge clk); //exec
-        readdata <= 32'h8C030001; //lw v1 1(zero) (loads value at address == 1)
+        readdata <= 32'h8C030001; //lw v1 0x1(zero) (loads value at address == 1)
         // Use https://www.eg.bucknell.edu/~csci320/mips_web/ to convert assembly to hex
         waitrequest <= 0;
 
@@ -76,7 +83,7 @@ module mips_cpu_bus_tb;
         assert(address==32'hBFC00008) else $fatal(102, "%s %s Fail CPU accessing incorrect address %h", TESTCASE_ID, INSTRUCTION, address);
 
         @(posedge clk); //exec
-        readdata <= 32'h24430000; //addiu v1 v0 0
+        readdata <= 32'h24430000; //addiu v1 v0 0x0
 
         @(negedge clk);
         assert(register_v0==32'd192) assert(write==0) else $fatal(106, "%s %s Fail Incorrect value %d stored in v0." TESTCASE_ID, INSTRUCTION, register_v0);
