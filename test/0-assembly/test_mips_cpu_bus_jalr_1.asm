25a08d472dd6843b7e276788b45fdaccac1061de
sts219 <sts219@ic.ac.uk>
2020-12-17 14:34:13 +0000

    Fixed jalr_1 testbench

--- test/0-assembly/test_mips_cpu_bus_jalr_1.asm
+++ test/0-assembly/test_mips_cpu_bus_jalr_1.asm
@@ -1,14 +1,13 @@
 LUI $v1 0xBFC0
-LW $t0 0x0028 $v1
-JALR $ra $t0
-ADDIU $ra $ra 0x0001
-ADDIU $ra $ra 0x0001
-ADD   $v0 $v0 $ra
-JR $zero
+LW $t0 0x0028($v1)
+JALR $v0 $t0
+nop
+jr $zero
+addiu $v0 $v0 0x1
 
+0x50: jr $zero
+      addiu $v0 $v0 0x2
 
+mem location from line 2: BFC00050
 
-
-mem location from line 2: BFC00005
-
-assert v0 = 0000000A
+assert v0 = bfc00012
