a3ef78b4a10352bca6527c87f885958ec51297d1
myaaqoubi <my5918@imperial.ac.uk>
2020-12-17 16:02:41 +0000

    tb new format

--- test/0-assembly/test_mips_cpu_bus_sllv_0.asm
+++ test/0-assembly/test_mips_cpu_bus_sllv_0.asm
@@ -2,5 +2,5 @@ lui v1 0xbfc0
     lw t1 0x28(v1)
     lw t2 0x2c(v1)
     jr zero (jumps to address==0)
-    or v0 t1 t2
-    assert(register_v0==32'hFFFFFFFF)
+    sllv v0 t1 t2
+    assert(register_v0==32'h000FFFF0)
--- test/0-assembly/test_mips_cpu_bus_sltiu_0.asm
+++ test/0-assembly/test_mips_cpu_bus_sltiu_0.asm
@@ -1,6 +1,6 @@
 lui v1 0xbfc0
 lw t1 0x28(v1)
 jr zero
-sll v0 t1 0x4
-000917C0
+sltiu v0 t1 0x0
+
 # assert(register_v0==32'h00000000)
