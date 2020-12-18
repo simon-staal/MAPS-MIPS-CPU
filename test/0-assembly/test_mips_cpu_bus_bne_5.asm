289d3ea80bfe15d92de20e12c2f1f06b988227cc
sts219 <sts219@ic.ac.uk>
2020-12-18 15:14:33 +0000

    bne_5 works (packwards branch)

---
+++ test/0-assembly/test_mips_cpu_bus_bne_5.asm
@@ -0,0 +1,16 @@
+lui v1 0xbfc0
+bne v0 v1 0x27
+lui v1 0xbfc0
+lw v0 0x28(v1)
+jr zero
+nop
+
+0x50: jr zero
+      addiu v0 v0 0x1
+
+0xa0: bne v1 zero 0xfffeb (-20)
+      addiu v0 v0 0x2
+      jr zero
+      addiu v0 v0 0x5
+
+assert(register_v0==32'h00000003)
