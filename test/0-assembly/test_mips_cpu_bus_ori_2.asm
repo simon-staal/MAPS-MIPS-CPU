56640d5a964054d0989dc010650f72055ac2bba9
myaaqoubi <my5918@imperial.ac.uk>
2020-12-19 19:56:22 +0000

    asm for ori 1-3

--- test/0-assembly/test_mips_cpu_bus_ori_2.asm
+++ test/0-assembly/test_mips_cpu_bus_ori_2.asm
@@ -1,1 +1,7 @@
-0000ffff
+lui v1 0xbfc0
+lw t1 0x28(v1)
+jr zero
+ori v0 t1 0xffff
+
+# assert(register_v0==32'hBABAffff)
+
--- test/0-assembly/test_mips_cpu_bus_ori_3.asm
+++ test/0-assembly/test_mips_cpu_bus_ori_3.asm
@@ -1,1 +1,7 @@
-ffffffff
+
+lui v1 0xbfc0
+lw t1 0x28(v1)
+jr zero
+ori v0 t1 0x000f
+
+# assert(register_v0==32'hffffffff)
