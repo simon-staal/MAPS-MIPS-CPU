6c38167c6a3a8c6191b7cdb2cd063ae6b6a0351f
sts219 <sts219@ic.ac.uk>
2020-12-20 15:38:19 +0000

    lwl_4, checks fully overwriting word

--- test/0-assembly/test_mips_cpu_bus_lwl_4.asm
+++ test/0-assembly/test_mips_cpu_bus_lwl_4.asm
@@ -6,6 +6,6 @@ nop
 
 0xa0: lw $v0 0xF628($v1) (should go back to line 11)
       jr $zero
-      lwl v0  0xF62c($v1) (line 12, 1 byte)
+      lwl v0  0xF62f($v1) (line 12, 1 byte)
 
 assert v0 = 73aaa39c
