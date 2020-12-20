38003f56540605353b3de47271ea7f726757543d
sts219 <sts219@ic.ac.uk>
2020-12-20 15:53:11 +0000

    lwr_4 tests accessing last byte (and -ve offset)

---
+++ test/0-assembly/test_mips_cpu_bus_lwr_4.asm
@@ -0,0 +1,11 @@
+lui $v1 0xbfc0
+beq zero zero 0x26 (line 41)
+addiu v1 v1 0xa00 (v1 now at 2560 past reset vector = line 641)
+jr $zero
+nop
+
+0xa0: lw $v0 0xF628($v1) (should go back to line 11)
+      jr $zero
+      lwl v0  0xF62f($v1) (line 12, last byte)
+
+assert v0 = 40aaa36d
