2c6102cec923c884bcedb1676151f9e06c57693a
sts219 <sts219@ic.ac.uk>
2020-12-20 15:33:06 +0000

    Updated lwl_2 tb

--- test/0-assembly/test_mips_cpu_bus_lwl_2.asm
+++ test/0-assembly/test_mips_cpu_bus_lwl_2.asm
@@ -1,9 +1,9 @@
 lui $v1 0xBfc0
 lw $v0 0x0028($v1)
-lwl $v0 0x002E($v1)
+lwl $v0 0x002d($v1)
 jr $zero
 sll $zero, $zero, 0x0000
 
 #BCF00028=076d589c
 #BCF0002C=1a2b3e1f
-#assert(v0==2b3e1f9c)
+#assert(v0==3e1f589c)
